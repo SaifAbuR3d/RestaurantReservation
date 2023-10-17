27b1549d3042064513ccdf46dad8797f29cd2743
SaifAbuR3d <saefraed2@gmail.com>
2023-10-17 12:13:00 +0300

    Create Domain project and Add reference from data access project to domain project

--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -4,6 +4,16 @@ namespace RestaurantReservation.Db
 {
     public class RestaurantReservationDbContext : DbContext
     {
+        private readonly string _connectionString;
 
+        public RestaurantReservationDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
     }
 }
