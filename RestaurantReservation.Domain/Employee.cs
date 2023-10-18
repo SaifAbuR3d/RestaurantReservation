2b83399f3def1ea776c9cd06cbafc2a5952a3c09
SaifAbuR3d <saefraed2@gmail.com>
2023-10-18 19:02:41 +0300

    Create Basic Domain Model

---
+++ RestaurantReservation.Domain/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Domain
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public int RestaurantId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string position { get; set; }
+        public Restaurant Restaurant { get; set; }
+        public List<Order> Orders { get; set; }
+
+    }
+}
