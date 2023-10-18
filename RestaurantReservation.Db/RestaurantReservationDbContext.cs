c50d4ae97f22946c8dd6eb092296ec33ad57042b
SaifAbuR3d <saefraed2@gmail.com>
2023-10-18 19:04:12 +0300

    Add DbSet properties to the DbContext

--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -1,10 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Domain;
 
 namespace RestaurantReservation.Db
 {
     public class RestaurantReservationDbContext : DbContext
     {
         private readonly string _connectionString;
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Table> Tables { get; set; }
+
+
 
         public RestaurantReservationDbContext(string connectionString)
         {
