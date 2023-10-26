3269647057cbff0da916e35a173ae152562328cb
SaifAbuR3d <saefraed2@gmail.com>
2023-10-26 09:56:56 +0300

    Implement RestaurantRepository contains basic Create/Update/Delete

---
+++ RestaurantReservation.Db/RestaurantRepository.cs
@@ -0,0 +1,38 @@
+﻿using RestaurantReservation.Domain;
+
+namespace RestaurantReservation.Db
+{
+    public class RestaurantRepository
+    {
+        private readonly RestaurantReservationDbContext _context;
+
+        public RestaurantRepository(RestaurantReservationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Restaurant CreateRestaurant(Restaurant restaurant)
+        {
+            _context.Restaurants.Add(restaurant);
+            _context.SaveChanges();
+            return restaurant;
+        }
+
+        public Restaurant UpdateRestaurant(Restaurant restaurant)
+        {
+            _context.Restaurants.Update(restaurant);
+            _context.SaveChanges();
+            return restaurant;
+        }
+
+        public void DeleteRestaurant(int restaurantId)
+        {
+            var restaurant = _context.Restaurants.Find(restaurantId);
+            if (restaurant != null)
+            {
+                _context.Restaurants.Remove(restaurant);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
