f0b7ae91dbb057b3a4cb48c75b22d5487b6d65b8
SaifAbuR3d <saefraed2@gmail.com>
2023-10-26 09:54:14 +0300

    Implement CustomerRepository contains basic Create/Update/Delete

---
+++ RestaurantReservation.Db/CustomerRepository.cs
@@ -0,0 +1,48 @@
+﻿using RestaurantReservation.Domain;
+
+namespace RestaurantReservation.Db
+{
+    public class CustomerRepository
+    {
+        private readonly RestaurantReservationDbContext _context;
+
+        public CustomerRepository(RestaurantReservationDbContext context) 
+        {
+            _context = context; 
+        }
+
+        public Customer CreateCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return customer;
+        }
+
+        public Customer UpdateCustomer(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            _context.SaveChanges();
+            return customer;
+        }
+
+        public void DeleteCustomer(int customerId)
+        {
+            var customer = _context.Customers.Find(customerId);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                _context.SaveChanges();
+            }
+        }
+
+        public void AddReservation(int customerId, Reservation reservation)
+        {
+            var customer = _context.Customers.Find(customerId);
+            if (customer != null)
+            {
+                customer.Reservations.Add(reservation);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
