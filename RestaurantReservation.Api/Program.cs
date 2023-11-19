d3fc028c3eb68980e6218fd1baee37114e9d0295
SaifAbuR3d <saefraed2@gmail.com>
2023-11-19 08:13:03 +0200

    Add Middlewares and Services in Program.cs and appsettings.json with connection string to SqlServer instance

---
+++ RestaurantReservation.Api/Program.cs
@@ -0,0 +1,35 @@
+using RestaurantReservation.Db;
+using RestaurantReservation.Db.Repositories;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+                .AddJsonOptions(opt =>
+                opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles)
+                .AddNewtonsoftJson(opt =>
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);  
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped(_ =>
+new RestaurantReservationDbContext(builder.Configuration.GetConnectionString("SqlServer")));
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
