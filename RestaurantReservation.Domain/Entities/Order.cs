c8bd1acb69b2d1104f9c473fb356f73a750a92c1
SaifAbuR3d <saefraed2@gmail.com>
2023-11-21 04:36:21 +0200

    Make TotalAmount in Order Entity Nullable

--- RestaurantReservation.Domain/Entities/Order.cs
+++ RestaurantReservation.Domain/Entities/Order.cs
@@ -6,22 +6,9 @@ public class Order
     public int ReservationId { get; set; }
     public int EmployeeId { get; set; }
     public DateTime OrderDate { get; set; }
-    public decimal TotalAmount { get; set; }
+    public decimal? TotalAmount { get; set; }
     public Reservation Reservation { get; set; }
     public Employee Employee { get; set; }
     public List<OrderItem> OrderItems { get; set; }
-
-    //public Order(ICollection<OrderItem> orderItems, Reservation reservation)
-    //{
-    //    OrderItems = new List<OrderItem>(orderItems);
-    //    Reservation = reservation
-    //    ReservationId = reservation.Id
-    //    CalculateTotalAmount();
-    //}
-
-    private void CalculateTotalAmount()
-    {
-        TotalAmount = OrderItems.Sum(orderItem => orderItem.Quantity * orderItem.MenuItem.Price);
-    }
 
 }
