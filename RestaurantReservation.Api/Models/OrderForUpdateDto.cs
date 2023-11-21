6f8097e332bdd6797f11acf1a40d21a687fafd6e
SaifAbuR3d <saefraed2@gmail.com>
2023-11-21 04:37:59 +0200

    Implement OrdersController and rewrite OrdersRepository

---
+++ RestaurantReservation.Api/Models/OrderForUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RestaurantReservation.Api.Models;
+
+public class OrderForUpdateDto
+{
+    public int ReservationId { get; set; }
+    public int EmployeeId { get; set; }
+    public DateTime OrderDate { get; set; }
+}
